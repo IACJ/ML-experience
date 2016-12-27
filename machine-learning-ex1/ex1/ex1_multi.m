96aca80135dedd8458d34e569efc03d9352722e1
ACJ <178480641@qq.com>
2016-12-27 19:00:17 +0800

    ex1 is finished

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,7 +82,7 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 0.1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
@@ -104,7 +104,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = theta(1)+ theta(2)*(1650-mu(1))/sigma(1)+ theta(3)*(3-mu(2))/sigma(2); % You should change this
 
 
 % ============================================================
@@ -149,7 +149,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = theta(1)+ theta(2)*1650+ theta(3)*3; % You should change this
 
 
 % ============================================================
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,10 +17,12 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+    h = X * theta;
 
 
+    delta = 1 / m  * (X' * (h-y));
 
-
+    theta = theta - alpha * delta ;
 
 
 
